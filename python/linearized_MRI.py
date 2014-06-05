2490c56802f7f6f8f91a926806bda5097943dad9
Susan Clark <sec2170@columbia.edu>
2014-06-05 10:39:06 -0400

    parameter space exploration 3

--- python/linearized_MRI.py
+++ python/linearized_MRI.py
@@ -79,8 +79,8 @@ def paramsearch():
     #Rmsearch = np.arange(4.88, 5.0, 0.001)
     #Qsearch = np.arange(0.745, 0.755, 0.001)
     #Rmsearch = np.arange(4.85, 4.95, 0.01)
-    Qsearch = np.arange(0.755, 0.765, 0.001)
-    Rmsearch = np.arange(4.95, 5.01, 0.01)
+    Qsearch = np.arange(0.745, 0.755, 0.001)
+    Rmsearch = np.arange(5.01, 5.06, 0.01)
     
     esearch = np.zeros((len(Qsearch), len(Rmsearch)), np.complex128)
     Rms = np.zeros((len(Qsearch), len(Rmsearch)), np.float_)
@@ -101,7 +101,7 @@ def paramsearch():
     
 esearch, Qs, Rms = paramsearch()
 
-np.save("esearch_2.npy", esearch)
-np.save("Qsearch_2.npy", Qs)
-np.save("Rmsearch_2.npy", Rms)
+np.save("esearch_3.npy", esearch)
+np.save("Qsearch_3.npy", Qs)
+np.save("Rmsearch_3.npy", Rms)
 
