a1c75823fad5eecbf076440eebad26e30d8bcc65
Susan Clark <sec2170@columbia.edu>
2014-06-10 11:36:57 -0400

    added coarse grid figure

--- python/runsplottest.py
+++ python/runsplottest.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pylab
 import pickle
 
-testdata = pickle.load(open("results_finegrid6.p", "rb"))
+testdata = pickle.load(open("results_test.p", "rb"))
 
 Rms = np.zeros(len(testdata))
 Qs = np.zeros(len(testdata))
