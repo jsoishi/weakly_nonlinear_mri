f781caa1b5788356dedcb1b17d4684381ab3c22c
Susan Clark <sec2170@columbia.edu>
2014-06-20 13:41:11 -0400

    typo

--- python/adjoint_homogenous.py
+++ python/adjoint_homogenous.py
@@ -39,7 +39,7 @@ def setup():
     #Correct equations
     lv1.add_equation("-1j*Q**2*dt(psi) + 1j*dt(psixx) + 1j*Q*A + 1j*(q - 2)*Q*u + iR*Q**4*psi - 2*iR*Q**2*psixx + iR*dx(psixxx) = 0")
     lv1.add_equation("1j*dt(u) + 1j*Q*B + 2*1j*Q*psi - iR*Q**2*u + iR*dx(ux) = 0")
-    lv1.add_equation("1j*dt(A) - iRm*Q**2*A + iRm*dx(Ax) - 1j*Q*q*B - 1j*Co*Q**3*psi + 1j*Co*Q*dx(psix) = 0")
+    lv1.add_equation("1j*dt(A) - iRm*Q**2*A + iRm*dx(Ax) - 1j*Q*q*B - 1j*Co*Q**3*psi + 1j*Co*Q*psixx = 0")
     lv1.add_equation("1j*dt(B) - iRm*Q**2*B + iRm*dx(Bx) + 1j*Co*Q*u = 0")
 
     lv1.add_equation("dx(psi) - psix = 0")
@@ -99,26 +99,46 @@ def plot_all(x, LEV):
 
     #Normalized to resemble Umurhan+
     norm1 = -0.9/np.min(LEV.state['u']['g'].imag) #norm(LEV.eigenvectors)
-
+    
     fig = plt.figure()
     ax1 = fig.add_subplot(221)
     ax1.plot(x, LEV.state['psi']['g'].imag*norm1, color="red")
     ax1.plot(x, LEV.state['psi']['g'].real*norm1, color="black")
+    
+    #psi_cc = LEV.state['psi']['g'] + LEV.state['psi']['g'].conj()
+    #ax1.plot(x, psi_cc.imag, color="red")
+    #ax1.plot(x, psi_cc.real, color="black")
+    
     ax1.set_title(r"Im($\psi^\dagger$)")
 
     ax2 = fig.add_subplot(222)
     ax2.plot(x, LEV.state['u']['g'].imag*norm1, color="black")
     ax2.plot(x, LEV.state['u']['g'].real*norm1, color="red")
+    
+    #u_cc = LEV.state['u']['g'] + LEV.state['u']['g'].conj()
+    #ax2.plot(x, u_cc.imag, color="black")
+    #ax2.plot(x, u_cc.real, color="red")
+    
     ax2.set_title("Re($u^\dagger$)")
 
     ax3 = fig.add_subplot(223)
     ax3.plot(x, LEV.state['A']['g'].imag*norm1, color="black")
     ax3.plot(x, LEV.state['A']['g'].real*norm1, color="red")
+    
+    #A_cc = LEV.state['A']['g'] + LEV.state['A']['g'].conj()
+    #ax3.plot(x, A_cc.imag, color="black")
+    #ax3.plot(x, A_cc.real, color="red")
+    
     ax3.set_title("Re($A^\dagger$)")
 
     ax4 = fig.add_subplot(224)
     ax4.plot(x, LEV.state['B']['g'].imag*norm1, color="red")
     ax4.plot(x, LEV.state['B']['g'].real*norm1, color="black")
+    
+    #B_cc = LEV.state['B']['g'] + LEV.state['B']['g'].conj()
+    #ax4.plot(x, B_cc.imag, color="red")
+    #ax4.plot(x, B_cc.real, color="black")
+    
     ax4.set_title("Im($B^\dagger$)")
     fig.savefig("ah1.png")
 
