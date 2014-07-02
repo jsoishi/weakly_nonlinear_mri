93872156718f8899478dde6a8b517eccac809087
Susan Clark <sec2170@columbia.edu>
2014-07-02 12:06:54 -0400

    second order nonlinear terms

--- python/allorders.py
+++ python/allorders.py
@@ -28,7 +28,7 @@ class AdjointHomogenous():
         self.beta = beta
         
     
-    def solve(self, gridnum = 64, save = True):
+    def solve(self, gridnum = 64, save = False):
 
         self.gridnum = gridnum
 
@@ -158,7 +158,7 @@ class OrderE():
         self.beta = beta
         
     
-    def solve(self, gridnum = 64, save = True):
+    def solve(self, gridnum = 64, save = False):
 
         self.gridnum = gridnum
 
@@ -281,6 +281,7 @@ class N2():
         self.q = q
         self.beta = beta
         
+        # either run or load the data in from saved files (needs implementing)
         if run == True:
             v1 = OrderE()
             v1.solve(save=False)
@@ -326,8 +327,6 @@ class N2():
         u_1 = self.u_1
         B_1 = self.B_1
         
-        print(A_1['g'])
-        
         psi_1_x = psi_1.differentiate(0)
         psi_1_xx = psi_1_x.differentiate(0)
         psi_1_xxx = psi_1_xx.differentiate(0)
@@ -340,42 +339,113 @@ class N2():
         
         B_1_x = B_1.differentiate(0)
         
+        # complex conjugates domain.new_field()
+        psi_1_star = domain.new_field()
+        psi_1_star.name = 'psi_1_star'
+        psi_1_star.data = psi_1['g'].conj()
+        psi_1_star_x = psi_1_star.differentiate(0)
+        psi_1_star_xx = psi_1_star_x.differentiate(0)
+        psi_1_star_xxx = psi_1_star_xx.differentiate(0)
         
-        N2psi = 1j*Q*psi_1*psi_1_xxx - 1j*Q*psi_1*Q**2*psi_1_xx - psi_1_x*1j*Q*psi_1_xx + psi_1_x*Q**2*psi_1 - (2/self.beta)*1j*Q*A_1*A_1_xxx + (2/self.beta)*1j*Q*A_1*Q**2*A_1_x + (2/self.beta)*A_1_x*1j*Q*A_1_xx - (2/self.beta)*A_1_x*Q**2*A_1
-        self.N2_psi = N2psi.evaluate()
         
-        N2u = 1j*Q*psi_1*u_1_x - psi_1_x*1j*Q*u_1 - (2/self.beta)*1j*Q*A_1*B_1_x + (2/self.beta)*A_1_x*1j*Q*B_1
-        self.N2_u = N2u.evaluate()
+        u_1_star = domain.new_field()
+        u_1_star.name = 'u_1_star'
+        u_1_star.data = u_1['g'].conj()
+        u_1_star_x = u_1_star.differentiate(0)
         
-        N2A = -1j*Q*A_1*psi_1_x + A_1_x*1j*Q*psi_1
-        self.N2_A = N2A.evaluate()
+        A_1_star = domain.new_field()
+        A_1_star.name = 'A_1_star'
+        A_1_star.data = A_1['g'].conj()
+        A_1_star_x = A_1_star.differentiate(0)
+        A_1_star_xx = A_1_star_x.differentiate(0)
+        A_1_star_xxx = A_1_star_xx.differentiate(0)
         
-        N2B = 1j*Q*psi_1*B_1_x - psi_1_x*1j*Q*B_1 - 1j*Q*A_1*u_1_x + A_1_x*1j*Q*u_1
-        self.N2_B = N2B.evaluate()
+        B_1_star = domain.new_field()
+        B_1_star.name = 'B_1_star'
+        B_1_star.data = B_1['g'].conj()
+        B_1_star_x = B_1_star.differentiate(0)
+        
+        # define nonlinear terms N22 and N20
+        N22psi = 1j*Q*psi_1*(psi_1_xxx - Q**2*psi_1_x) - psi_1_x*(1j*Q*psi_1_xx - 1j*Q**3*psi_1) + (2/self.beta)*A_1_x*(1j*Q*A_1_xx - 1j*Q**3*A_1) - (2/self.beta)*1j*Q*A_1*(A_1_xxx - Q**2*A_1_x)
+        self.N22_psi = N22psi.evaluate()
+        
+        N20psi = 1j*Q*psi_1*(psi_1_star_xxx - Q**2*psi_1_star_x) - psi_1_x*(1j*Q*psi_1_star_xx - 1j*Q**3*psi_1_star) + (2/self.beta)*A_1_x*(1j*Q*A_1_star_xx - 1j*Q**3*A_1_star) - (2/self.beta)*1j*Q*A_1*(A_1_star_xxx - Q**2*A_1_star_x)
+        self.N20_psi = N20psi.evaluate()
+        
+        N22u = 1j*Q*psi_1*u_1_x - psi_1_x*1j*Q*u_1 - (2/self.beta)*1j*Q*A_1*B_1_x + (2/self.beta)*A_1_x*1j*Q*B_1
+        self.N22_u = N22u.evaluate()
+        
+        N20u = 1j*Q*psi_1*u_1_star_x - psi_1_x*1j*Q*u_1_star - (2/self.beta)*1j*Q*A_1*B_1_star_x + (2/self.beta)*A_1_x*1j*Q*B_1_star
+        self.N20_u = N20u.evaluate()
+        
+        N22A = -1j*Q*A_1*psi_1_x + A_1_x*1j*Q*psi_1
+        self.N22_A = N22A.evaluate()
+        
+        N20A = -1j*Q*A_1*psi_1_star_x - A_1_x*1j*Q*psi_1_star
+        self.N20_A = N20A.evaluate()
+        
+        N22B = 1j*Q*psi_1*B_1_x - psi_1_x*1j*Q*B_1 - 1j*Q*A_1*u_1_x + A_1_x*1j*Q*u_1
+        self.N22_B = N22B.evaluate()
+        
+        N20B = 1j*Q*psi_1*B_1_star_x - psi_1_x*1j*Q*B_1_star - 1j*Q*A_1*u_1_star_x + A_1_x*1j*Q*u_1_star
+        self.N20_B = N20B.evaluate()
+        
+        
+        #N2psi = 1j*Q*psi_1*psi_1_xxx - 1j*Q*psi_1*Q**2*psi_1_xx - psi_1_x*1j*Q*psi_1_xx + psi_1_x*Q**2*psi_1 - (2/self.beta)*1j*Q*A_1*A_1_xxx + (2/self.beta)*1j*Q*A_1*Q**2*A_1_x + (2/self.beta)*A_1_x*1j*Q*A_1_xx - (2/self.beta)*A_1_x*Q**2*A_1
+        #self.N2_psi = N2psi.evaluate()
+        
+        #N2u = 1j*Q*psi_1*u_1_x - psi_1_x*1j*Q*u_1 - (2/self.beta)*1j*Q*A_1*B_1_x + (2/self.beta)*A_1_x*1j*Q*B_1
+        #self.N2_u = N2u.evaluate()
+        
+        #N2A = -1j*Q*A_1*psi_1_x + A_1_x*1j*Q*psi_1
+        #self.N2_A = N2A.evaluate()
+        
+        #N2B = 1j*Q*psi_1*B_1_x - psi_1_x*1j*Q*B_1 - 1j*Q*A_1*u_1_x + A_1_x*1j*Q*u_1
+        #self.N2_B = N2B.evaluate()
         
     def plot(self):
     
         fig = plt.figure()
         
-        ax1 = fig.add_subplot(221)
-        ax1.plot(self.x, self.N2_psi['g'].imag, color="black")
-        ax1.plot(self.x, self.N2_psi['g'].real, color="red")
-        ax1.set_title(r"Im($\psi_{N2}$)")
+        ax1 = fig.add_subplot(241)
+        ax1.plot(self.x, self.N20_psi['g'].imag, color="black")
+        ax1.plot(self.x, self.N20_psi['g'].real, color="red")
+        ax1.set_title(r"Im($\psi_{N20}$)")
 
-        ax2 = fig.add_subplot(222)
-        ax2.plot(self.x, self.N2_u['g'].real, color="black")
-        ax2.plot(self.x, self.N2_u['g'].imag, color="red")
-        ax2.set_title("Re($u_{N2}$)")
+        ax2 = fig.add_subplot(242)
+        ax2.plot(self.x, self.N20_u['g'].real, color="black")
+        ax2.plot(self.x, self.N20_u['g'].imag, color="red")
+        ax2.set_title("Re($u_{N20}$)")
 
-        ax3 = fig.add_subplot(223)
-        ax3.plot(self.x, self.N2_A['g'].real, color="black")
-        ax3.plot(self.x, self.N2_A['g'].imag, color="red")
-        ax3.set_title("Re($A_{N2}$)")
+        ax3 = fig.add_subplot(243)
+        ax3.plot(self.x, self.N20_A['g'].real, color="black")
+        ax3.plot(self.x, self.N20_A['g'].imag, color="red")
+        ax3.set_title("Re($A_{N20}$)")
 
-        ax4 = fig.add_subplot(224)
-        ax4.plot(self.x, self.N2_B['g'].imag, color="black")
-        ax4.plot(self.x, self.N2_B['g'].real, color="red")
-        ax4.set_title("Im($B_{N2}$)")
+        ax4 = fig.add_subplot(244)
+        ax4.plot(self.x, self.N20_B['g'].imag, color="black")
+        ax4.plot(self.x, self.N20_B['g'].real, color="red")
+        ax4.set_title("Im($B_{N20}$)")
+        
+        ax1 = fig.add_subplot(245)
+        ax1.plot(self.x, self.N22_psi['g'].imag, color="black")
+        ax1.plot(self.x, self.N22_psi['g'].real, color="red")
+        ax1.set_title(r"Im($\psi_{N22}$)")
+
+        ax2 = fig.add_subplot(246)
+        ax2.plot(self.x, self.N22_u['g'].real, color="black")
+        ax2.plot(self.x, self.N22_u['g'].imag, color="red")
+        ax2.set_title("Re($u_{N22}$)")
+
+        ax3 = fig.add_subplot(247)
+        ax3.plot(self.x, self.N22_A['g'].real, color="black")
+        ax3.plot(self.x, self.N22_A['g'].imag, color="red")
+        ax3.set_title("Re($A_{N22}$)")
+
+        ax4 = fig.add_subplot(248)
+        ax4.plot(self.x, self.N22_B['g'].imag, color="black")
+        ax4.plot(self.x, self.N22_B['g'].real, color="red")
+        ax4.set_title("Im($B_{N22}$)")
         fig.savefig("n2.png")
 
         
@@ -405,10 +475,15 @@ class OrderE2():
             
             n2 = N2()
             n2.solve()
-            self.n2_psi = n2.N2_psi
-            self.n2_u = n2.N2_u
-            self.n2_A = n2.N2_A
-            self.n2_B = n2.N2_B
+            self.n22_psi = n2.N22_psi
+            self.n22_u = n2.N22_u
+            self.n22_A = n2.N22_A
+            self.n22_B = n2.N22_B
+            
+            self.n20_psi = n2.N20_psi
+            self.n20_u = n2.N20_u
+            self.n20_A = n2.N20_A
+            self.n20_B = n2.N20_B
             
     def solve(self, gridnum = 64, save = True):
     
@@ -444,42 +519,55 @@ class OrderE2():
         self.term2_B = term2_B.evaluate()
         
         # righthand side
-        n2_psi = self.n2_psi
-        term2_psi = self.term2_psi
-        rhs_psi = n2_psi + term2_psi
-        self.rhs_psi = rhs_psi.evaluate()
+        #n2_psi = self.n2_psi
+        #term2_psi = self.term2_psi
+        #rhs_psi = n2_psi + term2_psi
+        #self.rhs_psi = rhs_psi.evaluate()
         
-        rhs_u = self.n2_u + self.term2_u
-        self.rhs_u = rhs_u.evaluate()
+        #rhs_u = self.n2_u + self.term2_u
+        #self.rhs_u = rhs_u.evaluate()
         
-        rhs_A = self.n2_A + self.term2_A
-        self.rhs_A = rhs_A.evaluate()
+        #rhs_A = self.n2_A + self.term2_A
+        #self.rhs_A = rhs_A.evaluate()
         
-        rhs_B = self.n2_B + self.term2_B
-        self.rhs_B = rhs_B.evaluate()
+        #rhs_B = self.n2_B + self.term2_B
+        #self.rhs_B = rhs_B.evaluate()
         
         # nonconstant coefficients
-        rhs_psi = self.rhs_psi['g']
-        rhs_u = self.rhs_u['g']
-        rhs_A = self.rhs_A['g']
-        rhs_B = self.rhs_B['g']
+        #rhs_psi = self.rhs_psi['g']
+        #rhs_u = self.rhs_u['g']
+        #rhs_A = self.rhs_A['g']
+        #rhs_B = self.rhs_B['g']
         
-        # should these instead be just term2 for the 21 terms?
-        rhs_psi = self.term2_psi['g']
-        rhs_u = self.term2_u['g']
-        rhs_A = self.term2_A['g']
-        rhs_B = self.term2_B['g']
+        # righthand side for the 21 terms (e^iQz)
+        rhs21_psi = self.term2_psi['g']
+        rhs21_u = self.term2_u['g']
+        rhs21_A = self.term2_A['g']
+        rhs21_B = self.term2_B['g']
         
-        rhs_n2_psi = self.n2_psi['g']
-        rhs_n2_u = self.n2_u['g']
-        rhs_n2_A = self.n2_A['g']
-        rhs_n2_B = self.n2_B['g']        
+        #rhs_n2_psi = self.n2_psi['g']
+        #rhs_n2_u = self.n2_u['g']
+        #rhs_n2_A = self.n2_A['g']
+        #rhs_n2_B = self.n2_B['g']
+        
+        # righthand side for the 22 terms (e^2iQz)
+        rhs22_psi = self.n22_psi['g']
+        rhs22_u = self.n22_u['g']
+        rhs22_A = self.n22_A['g']
+        rhs22_B = self.n22_B['g']
+        
+        # righthand side for the 20 terms (e^0)
+        rhs20_psi = self.n20_psi['g']
+        rhs20_u = self.n20_u['g']
+        rhs20_A = self.n20_A['g']
+        rhs20_B = self.n20_B['g']
+                
         
         # define problem using righthand side as nonconstant coefficients
         
         lv2 = ParsedProblem(['x'],
                               field_names=['psi20', 'psi20x', 'psi20xx', 'psi20xxx', 'u20', 'u20x', 'A20', 'A20x', 'B20', 'B20x', 'psi21', 'psi21x', 'psi21xx', 'psi21xxx', 'u21', 'u21x', 'A21', 'A21x', 'B21', 'B21x', 'psi22', 'psi22x', 'psi22xx', 'psi22xxx', 'u22', 'u22x', 'A22', 'A22x', 'B22', 'B22x'],
-                              param_names=['Q', 'iR', 'iRm', 'q', 'beta', 'rhs_psi', 'rhs_u', 'rhs_A', 'rhs_B'])
+                              param_names=['Q', 'iR', 'iRm', 'q', 'beta', 'rhs20_psi', 'rhs20_u', 'rhs20_A', 'rhs20_B', 'rhs21_psi', 'rhs21_u', 'rhs21_A', 'rhs21_B', 'rhs22_psi', 'rhs22_u', 'rhs22_A', 'rhs22_B'])
                   
         #x_basis = Chebyshev(gridnum)
         #domain = Domain([x_basis], grid_dtype=np.complex128)
@@ -491,18 +579,18 @@ class OrderE2():
         #lv2.add_equation("1j*dt(B20) + 1j*dt(B21) + 1j*dt(B22) + -q*A20 + 1j*Q*q*A21 - 2*1j*Q*q*A22 + iRm*dx(B20x) + iRm*B20 + iRm*Q**2*B21 - iRm*dx(B21x) - iRm*4*Q**2*B22 + iRm*dx(B22x) - 1j*Q*u21 + 2*1j*Q*u22 + u20 = rhs_B")
         
         # need to 'stack' equations for each component of V2 in order to have 30 equations
-        lv2.add_equation("1j*dt(psi20xx) + 1j*dt(psi20) + (2/beta)*dx(A20x) + (2/beta)*A20 + iR*dx(psi20xxx) + 2*iR*psi20xx + iR*psi20 + 2*u20 = rhs_n2_psi")
-        lv2.add_equation("1j*dt(u20) + (2/beta)*B20 + (q - 2)*psi20 + iR*dx(u20x) + iR*u20 = rhs_n2_u")
-        lv2.add_equation("1j*dt(A20) + iRm*dx(A20x) + iRm*A20 + psi20 = rhs_n2_A")
-        lv2.add_equation("1j*dt(B20) + -q*A20 + iRm*dx(B20x) + iRm*B20 + u20 = rhs_n2_B")
-        lv2.add_equation("1j*Q**2*dt(psi21) - 1j*dt(psi21xx) + 1j*(2/beta)*Q**3*A21 - 1j*(2/beta)*Q*dx(A21x) - 2*1j*Q*u21 - iR*Q**4*psi21 + 2*iR*Q**2*psi21xx - iR*dx(psi21xxx) = rhs_psi")
-        lv2.add_equation("-1j*dt(u21) + -1j*(2/beta)*Q*B21 - 1j*Q*(q - 2)*psi21 + iR*Q**2*u21 - iR*dx(u21x) = rhs_u")
-        lv2.add_equation("-1j*dt(A21) + iRm*Q**2*A21 - iRm*dx(A21x) - 1j*Q*psi21 = rhs_A")
-        lv2.add_equation("-1j*dt(B21) + 1j*Q*q*A21 + iRm*Q**2*B21 - iRm*dx(B21x) - 1j*Q*u21 = rhs_B")
-        lv2.add_equation("-4*Q**2*1j*dt(psi22) + 1j*dt(psi22xx) + -8*1j*(2/beta)*Q**3*A22 + 2*1j*(2/beta)*Q*dx(A22x) + 4*1j*Q*u22 + 16*iR*Q**4*psi22 - 8*iR*Q**2*psi22xx + iR*dx(psi22xxx) = rhs_n2_psi")
-        lv2.add_equation("1j*dt(u22) + 2*1j*(2/beta)*Q*B22 + 2*1j*Q*(q-2)*psi22 - 4*iR*Q**2*u22 + iR*dx(u22x) = rhs_n2_u")
-        lv2.add_equation("1j*dt(A22) + -iRm*4*Q**2*A22 + iRm*dx(A22x) + 2*1j*Q*psi22 = rhs_n2_A")
-        lv2.add_equation("1j*dt(B22) + -2*1j*Q*q*A22 - iRm*4*Q**2*B22 + iRm*dx(B22x) + 2*1j*Q*u22 = rhs_n2_B")
+        lv2.add_equation("1j*dt(psi20xx) + 1j*dt(psi20) + (2/beta)*dx(A20x) + (2/beta)*A20 + iR*dx(psi20xxx) + 2*iR*psi20xx + iR*psi20 + 2*u20 = rhs20_psi")
+        lv2.add_equation("1j*dt(u20) + (2/beta)*B20 + (q - 2)*psi20 + iR*dx(u20x) + iR*u20 = rhs20_u")
+        lv2.add_equation("1j*dt(A20) + iRm*dx(A20x) + iRm*A20 + psi20 = rhs20_A")
+        lv2.add_equation("1j*dt(B20) + -q*A20 + iRm*dx(B20x) + iRm*B20 + u20 = rhs20_B")
+        lv2.add_equation("1j*Q**2*dt(psi21) - 1j*dt(psi21xx) + 1j*(2/beta)*Q**3*A21 - 1j*(2/beta)*Q*dx(A21x) - 2*1j*Q*u21 - iR*Q**4*psi21 + 2*iR*Q**2*psi21xx - iR*dx(psi21xxx) = rhs21_psi")
+        lv2.add_equation("-1j*dt(u21) + -1j*(2/beta)*Q*B21 - 1j*Q*(q - 2)*psi21 + iR*Q**2*u21 - iR*dx(u21x) = rhs21_u")
+        lv2.add_equation("-1j*dt(A21) + iRm*Q**2*A21 - iRm*dx(A21x) - 1j*Q*psi21 = rhs21_A")
+        lv2.add_equation("-1j*dt(B21) + 1j*Q*q*A21 + iRm*Q**2*B21 - iRm*dx(B21x) - 1j*Q*u21 = rhs21_B")
+        lv2.add_equation("-4*Q**2*1j*dt(psi22) + 1j*dt(psi22xx) + -8*1j*(2/beta)*Q**3*A22 + 2*1j*(2/beta)*Q*dx(A22x) + 4*1j*Q*u22 + 16*iR*Q**4*psi22 - 8*iR*Q**2*psi22xx + iR*dx(psi22xxx) = rhs22_psi")
+        lv2.add_equation("1j*dt(u22) + 2*1j*(2/beta)*Q*B22 + 2*1j*Q*(q-2)*psi22 - 4*iR*Q**2*u22 + iR*dx(u22x) = rhs22_u")
+        lv2.add_equation("1j*dt(A22) + -iRm*4*Q**2*A22 + iRm*dx(A22x) + 2*1j*Q*psi22 = rhs22_A")
+        lv2.add_equation("1j*dt(B22) + -2*1j*Q*q*A22 - iRm*4*Q**2*B22 + iRm*dx(B22x) + 2*1j*Q*u22 = rhs22_B")
         
                                         
         lv2.add_equation("dx(psi20) - psi20x = 0")
@@ -571,10 +659,18 @@ class OrderE2():
         lv2.parameters['iRm'] = iRm
         lv2.parameters['q'] = q
         lv2.parameters['beta'] = beta
-        lv2.parameters['rhs_psi'] = rhs_psi
-        lv2.parameters['rhs_u'] = rhs_u
-        lv2.parameters['rhs_A'] = rhs_A
-        lv2.parameters['rhs_B'] = rhs_B
+        lv2.parameters['rhs20_psi'] = rhs20_psi
+        lv2.parameters['rhs20_u'] = rhs20_u
+        lv2.parameters['rhs20_A'] = rhs20_A
+        lv2.parameters['rhs20_B'] = rhs20_B
+        lv2.parameters['rhs21_psi'] = rhs21_psi
+        lv2.parameters['rhs21_u'] = rhs21_u
+        lv2.parameters['rhs21_A'] = rhs21_A
+        lv2.parameters['rhs21_B'] = rhs21_B
+        lv2.parameters['rhs22_psi'] = rhs22_psi
+        lv2.parameters['rhs22_u'] = rhs22_u
+        lv2.parameters['rhs22_A'] = rhs22_A
+        lv2.parameters['rhs22_B'] = rhs22_B
 
         # expand domain to gridnum points
         lv2.expand(domain, order = gridnum)
