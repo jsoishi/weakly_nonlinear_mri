a8fd8b7c1adfd19d3cdb93078cf7d3708f4d6219
Susan Clark <sec2170@columbia.edu>
2014-09-19 16:12:19 -0400

    more appendix

--- python/allorders.py
+++ python/allorders.py
@@ -2394,7 +2394,7 @@ def plot_By_secondorder(pc_obj, oplot = True, labels = False):
         ax.set_ylabel("z (vertical)", size = 20)
         cbar.set_label(r"$B_y$ Perturbation", size = 20)
     
-    # only the first order perturbations
+    # only the second order perturbations
     V2_Bz1 = pc_obj.eps**2*pc_obj.V2_Bz1
     V2_Bx1 = pc_obj.eps**2*pc_obj.V2_Bx1
     
