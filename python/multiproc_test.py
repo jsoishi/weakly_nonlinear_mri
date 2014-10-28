58d9bb3bac3c2f474458bf00d855afd47c525fc2
Susan Clark <sec2170@columbia.edu>
2014-10-28 19:43:02 -0400

    debugging

--- python/multiproc_test.py
+++ python/multiproc_test.py
@@ -107,12 +107,12 @@ if __name__ == '__main__':
 
     with Pool(processes=15) as pool:
     
-        def cb(r, params):
+        def cb(r):
             print("callback")
             result[params] = r
             #return result[params]
     
-        def ec(r, params):
+        def ec(r):
             result[params] = np.nan
             #return result[params]
             #raise the error it got...
