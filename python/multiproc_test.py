0b4f714d3694aaaba43017a9b397f30590143c9c
Susan Clark <sec2170@columbia.edu>
2014-11-11 15:40:23 -0500

    return smallest negative and smallest positive eigenvalue

--- python/multiproc_test.py
+++ python/multiproc_test.py
@@ -271,10 +271,13 @@ def run_hmri_solve(Q, Pm, Rm, q, beta, run_id, xi, x0):
 
         val = evals[e0]
         
-        epos = evals[evals == np.nanmin(evals[evals > 0])]
-        eneg = evals[evals == np.nanmin(evals[evals < 0])]
+        epos = indx[evals == np.nanmin(evals[evals > 0])]
+        eneg = indx[evals == np.nanmin(evals[evals < 0])]
         
-        return (run_id, epos[0], eneg[0])
+        e_pos = evals[epos]
+        e_neg = evals[eneg]
+        
+        return (run_id, e_pos[0], e_neg[0])
         
     except np.linalg.LinAlgError:
         return (run_id, np.nan)
