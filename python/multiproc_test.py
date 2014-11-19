88e47d8f128e2e74e651713ee39a835b1bf73e99
Susan Clark <sec2170@columbia.edu>
2014-11-18 21:43:12 -0500

    fixed nan

--- python/multiproc_test.py
+++ python/multiproc_test.py
@@ -316,7 +316,7 @@ if __name__ == '__main__':
     #Qsearch = np.arange(0.0001, 0.4, dQ)
     
     #Rmsearch = np.arange(0.015, 0.016, dRm)
-    Rmsearch = np.arange(0.0, 0.1, dRm) 
+    Rmsearch = np.arange(0.005, 0.1, dRm) 
     Qsearch = np.arange(0.0, 10.0, dQ)
     Pmrun_hmri(Pm, q, beta, dQ, dRm, Qsearch, Rmsearch, xi, x0)
 
