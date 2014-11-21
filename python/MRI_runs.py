6371f2de40c218334314b9a6031102d2f8b1c5aa
Susan Clark <sec2170@columbia.edu>
2014-11-21 18:19:38 -0500

    pspace dir

---
+++ python/MRI_runs.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dedalus2.public import *
+from dedalus2.pde.solvers import LinearEigenvalue, LinearBVP
+from scipy.linalg import eig, norm
+import pylab
+import copy
+import pickle
+import plot_tools
+import streamplot_uneven as su
+import random
+from allorders import *
+
+import matplotlib
+matplotlib.rcParams['backend'] = "Qt4Agg"
+matplotlib.rcParams.update({'figure.autolayout': True})
+
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+
+gridnum = 256
+x_basis = Chebyshev(gridnum)
+domain = Domain([x_basis], grid_dtype=np.complex128)
+
+# Pm = 1E-6: Q = 0.75, Rm = 4.88, beta = 25
+"""
+Pm = 1.0E-6
+Q = 0.75
+Rm = 4.88
+q = 1.5
+beta = 25.0
+"""
+
+#Pm = 1E-7: Q = 0.745, Rm = 4.90
+Pm = 1.0E-7
+Q = 0.745
+Rm = 4.90
+q = 1.5
+beta = 25.0
+
+pc = PlotContours(Q = Q, Rm = Rm, Pm = Pm, q = q, beta = beta, run = True, norm = True)
+pc.plot_streams()
+pc.plot_Bfield()
+
+coeffs = {}
+coeffs["Q"] = Q
+coeffs["Rm"] = Rm
+coeffs["Pm"] = Pm
+coeffs["beta"] = beta
+coeffs["q"] = q
+coeffs["a"] = pc.saa.alpha_amp.a
+coeffs["c"] = pc.saa.alpha_amp.c
+coeffs["ctwiddle"] = pc.saa.alpha_amp.c_twiddle
+coeffs["b"] = pc.saa.alpha_amp.b
+coeffs["h"] = pc.saa.alpha_amp.h
+coeffs["g"] = pc.saa.alpha_amp.g
+
+outname = "gridnum_"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)
+plot_uy_firstorder(pc, oplot = True, labels = False, outname=outname)
+plot_By_firstorder(pc, oplot = True, labels = False, outname=outname)
+plot_uy_secondorder(pc, oplot = True, labels = False, outname=outname)
+plot_By_secondorder(pc, oplot = True, labels = False, outname=outname)
+plot_uy(pc, oplot = True, labels = False, outname=outname)
+plot_By(pc, oplot = True, labels = False, outname=outname)
+
+print(coeffs)
+
+pickle.dump(coeffs, open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "wb"))
