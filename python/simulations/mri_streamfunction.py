b48bbdc6b298c18e471879f2740a0c547dd46800
J.S. Oishi <jsoishi@gmail.com>
2014-11-28 12:46:40 -0500

    fixed streamfunction formalism BC by raising RHS order...

--- python/simulations/mri_streamfunction.py
+++ python/simulations/mri_streamfunction.py
@@ -22,7 +22,7 @@ z_basis = de.Fourier(gridnum)
 domain = de.Domain([z_basis,x_basis])
 
 mri = de.ParsedProblem(['z','x'],
-                       field_names=['psi','u','A','b','psi_x','psi_xx','psi_xxx','u_x','A_x','A_xx', 'b_x'],
+                       field_names=['psi','u','A','b','psi_x','psi_xx','psi_xxx','u_x','A_x','b_x'],
                        param_names=['Re','Rm','B0','Omega0','q','beta'])
 
 # Parameters
@@ -35,13 +35,13 @@ mri.parameters['beta'] = 25.0
 mri.parameters['Omega0'] = 1.
 
 #streamfunction
-mri.add_equation("dt(dx(psi_x)) + dz(dz(dt(psi))) - 2*dz(u) + (dx(psi_xxx) + dz(dz(dz(dz(psi)))))/Re - 2*B0/beta*(dz(A_xx) + dz(dz(dz(A)))) = 2/beta*((dx(A_xx) + dz(dz(A_x))) * dz(A) - (dz(A_xx) + dz(dz(dz(A))))*A_x) - ((psi_xxx + dz(dz(A_x))) * dz(psi) - (dz(psi_xx) + dz(dz(dz(psi))))*psi_x)")
+mri.add_equation("dt(dx(psi_x)) + dz(dz(dt(psi))) - 2*dz(u) + (dx(psi_xxx) + dz(dz(dz(dz(psi)))))/Re - 2*B0/beta*(dz(dx(A_x)) + dz(dz(dz(A)))) = 2/beta*((dx(dx(A_x)) + dz(dz(A_x))) * dz(A) - (dz(dx(A_x)) + dz(dz(dz(A))))*A_x) - ((psi_xxx + dz(dz(A_x))) * dz(psi) - (dz(psi_xx) + dz(dz(dz(psi))))*psi_x)")
 
 #u (y-velocity)
 mri.add_equation("dt(u) + (2-q)*Omega0*dz(psi) - 2*B0/beta * dz(b) - (dx(u_x) + dz(dz(u)))/Re = 2./beta*(dz(A) * b_x - A_x * dz(b))")
 
 #vector potential
-mri.add_equation("dt(A) - B0 * dz(psi) - (A_xx + dz(dz(A)))/Rm = dz(A) * psi_x - A_x * dz(psi)")
+mri.add_equation("dt(A) - B0 * dz(psi) - (dx(A_x) + dz(dz(A)))/Rm = dz(A) * psi_x - A_x * dz(psi)")
 
 #b (y-field)
 mri.add_equation("dt(b) - B0*dz(u) + q*Omega0 * dz(A) - (dx(b_x) + dz(dz(b)))/Rm = dz(A) * u_x - A_x * dz(u) - (b_x*dz(psi) - dz(b)*psi_x)")
@@ -52,7 +52,6 @@ mri.add_equation("psi_xx - dx(psi_x) = 0")
 mri.add_equation("psi_xxx - dx(psi_xx) = 0")
 mri.add_equation("u_x - dx(u) = 0")
 mri.add_equation("A_x - dx(A) = 0")
-mri.add_equation("A_xx - dx(A_x) = 0")
 mri.add_equation("b_x - dx(b) = 0")
 
 # ten boundary conditions
@@ -72,7 +71,7 @@ mri.add_right_bc("u = 0")
 mri.add_left_bc("dz(psi) = 0",condition="dz != 0")
 mri.add_right_bc("dz(psi) = 0",condition="dz != 0")
 mri.add_left_bc("psi = 0",condition="dz == 0")
-mri.add_right_bc("psi = 10",condition="dz == 0")
+mri.add_right_bc("psi = 0",condition="dz == 0")
 mri.add_left_bc("psi_x = 0")
 mri.add_right_bc("psi_x = 0")
 mri.add_left_bc("dz(A) = 0",condition="dz != 0")
