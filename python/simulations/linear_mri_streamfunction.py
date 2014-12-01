020e648217c4b24fb939693126c6563962cb1488
J.S. Oishi <jsoishi@gmail.com>
2014-12-01 15:36:31 -0500

    only psi works with psi = 0 BCs

--- python/simulations/linear_mri_streamfunction.py
+++ python/simulations/linear_mri_streamfunction.py
@@ -24,7 +24,7 @@ z_basis = de.Fourier(nz)
 domain = de.Domain([z_basis,x_basis])
 
 mri = de.ParsedProblem(['z','x'],
-                       field_names=['psi','u','A','b','psi_x','psi_xx','psi_xxx','u_x','A_x','b_x'],
+                       field_names=['psi','psi_x','psi_xx','psi_xxx'],
                        param_names=['Re','Rm','B0','Omega0','q','beta'])
 
 # Parameters
@@ -37,40 +37,18 @@ mri.parameters['beta'] = 25.0
 mri.parameters['Omega0'] = 1.
 
 #streamfunction
-mri.add_equation("dt(dx(psi_x)) + dz(dz(dt(psi))) - 2*dz(u) - (dx(psi_xxx) + dz(dz(dz(dz(psi)))))/Re - 2*B0/beta*(dz(dx(A_x)) + dz(dz(dz(A)))) = 0")
-
-#u (y-velocity)
-mri.add_equation("dt(u) + (2-q)*Omega0*dz(psi) - 2*B0/beta * dz(b) - (dx(u_x) + dz(dz(u)))/Re = 0")
-
-#vector potential
-mri.add_equation("dt(A) - B0 * dz(psi) - (dx(A_x) + dz(dz(A)))/Rm = 0")
-
-#b (y-field)
-mri.add_equation("dt(b) - B0*dz(u) + q*Omega0 * dz(A) - (dx(b_x) + dz(dz(b)))/Rm = 0")
+mri.add_equation("dt(dx(psi_x)) + dz(dz(dt(psi))) - (dx(psi_xxx) + dz(dz(dz(dz(psi)))))/Re = 0")
 
 # first-order scheme definitions
 mri.add_equation("psi_x - dx(psi) = 0")
 mri.add_equation("psi_xx - dx(psi_x) = 0")
 mri.add_equation("psi_xxx - dx(psi_xx) = 0")
-mri.add_equation("u_x - dx(u) = 0")
-mri.add_equation("A_x - dx(A) = 0")
-mri.add_equation("b_x - dx(b) = 0")
 
-# ten boundary conditions
-mri.add_left_bc("u = 0")
-mri.add_right_bc("u = 0")
-#mri.add_left_bc("dz(psi) = 0",condition="dz != 0")
-#mri.add_right_bc("dz(psi) = 0",condition="dz != 0")
+# four boundary conditions
 mri.add_left_bc("psi = 0")#,condition="dz == 0")
 mri.add_right_bc("psi = 0")#,condition="dz == 0")
 mri.add_left_bc("psi_x = 0")
 mri.add_right_bc("psi_x = 0")
-#mri.add_left_bc("dz(A) = 0",condition="dz != 0")
-#mri.add_right_bc("dz(A) = 0",condition="dz != 0")
-mri.add_left_bc("A = 0")#,condition="dz == 0")
-mri.add_right_bc("A = 0")#,condition="dz == 0")
-mri.add_left_bc("A_x = 0")
-mri.add_right_bc("A_x = 0")
 
 # prepare
 mri.expand(domain)
@@ -86,9 +64,6 @@ solver.stop_wall_time = 24*3600. # run for 24 hours
 
 # initial conditions
 psi = solver.state['psi']
-u = solver.state['u']
-A = solver.state['A']
-b = solver.state['b']
 
 # noise
 x = domain.grid(1)
@@ -102,9 +77,6 @@ output_iter_cadence = 1
 analysis_slice = solver.evaluator.add_file_handler(os.path.join(data_dir,"slices"), iter=output_iter_cadence, sim_dt=output_time_cadence, parallel=False)
 
 analysis_slice.add_task("psi", name="psi")
-analysis_slice.add_task("u", name="u")
-analysis_slice.add_task("A", name="A")
-analysis_slice.add_task("b", name="b")
 
 dt = 0.001
 start_time = time.time()
