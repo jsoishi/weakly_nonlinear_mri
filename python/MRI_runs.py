fixed sign errors in allorders, some new plotting tools

python/MRI_runs.py
@@ -19,7 +19,7 @@ from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 
-gridnum = 512
+gridnum = 64
 x_basis = Chebyshev(gridnum)
 domain = Domain([x_basis], grid_dtype=np.complex128)
 
@@ -33,22 +33,22 @@ beta = 25.0
 """
 
 #Pm = 1E-3: Q = 0.75, Rm = 4.8775
-"""
+
 Pm = 1.0E-3
 Q = 0.75
 Rm = 4.8775
 q = 1.5
 beta = 25.0
-"""
+
 
 #Pm = 1E-4: Q = 0.747, Rm = 4.88
-
+"""
 Pm = 1.0E-4
 Q = 0.747
 Rm = 4.88
 q = 1.5
 beta = 25.0
-
+"""
 
 #Pm = 1E-5, Q = 0.747, Rm = 4.88
 """
@@ -108,6 +108,7 @@ coeffs["h"] = pc.saa.alpha_amp.h
 coeffs["g"] = pc.saa.alpha_amp.g
 coeffs["t_array"] = pc.saa.t_array
 coeffs["alpha_array"] = pc.saa.alpha_array[:, 0]
+coeffs["alpha_s"] = pc.alpha_s
 coeffs["x"] = pc.saa.alpha_amp.x
 coeffs["eps"] = pc.eps
 coeffs["u_x first order"] = pc.V1_ux1

python/MRI_unpack.py
@@ -21,6 +21,8 @@ norm = False
 
 coeffs = {}
 
+path = "/Volumes/DataDavy/MRI/coeffs/"
+
 #Pm = 1E-7: Q = 0.745, Rm = 4.90
 """
 gridnum = 1024#512
@@ -30,14 +32,18 @@ Rm = 4.90
 
 coeffs[0] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "rb"))
 """
+
 """
+
 gridnum = 512
 # Pm = 1E-6: Q = 0.75, Rm = 4.88, beta = 25
 Pm = 1.0E-6
 Q = 0.75
 Rm = 4.88
+
 coeffs[1] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "rb"))
 """
+
 
 # all the rest have gridnum 256
 #gridnum = 256
@@ -49,21 +55,23 @@ Pm = 1.0E-5
 Q = 0.747
 Rm = 4.88
 
+
 if norm == False:
     coeffs[0] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+"_norm_"+str(norm)+".p", "rb"))
 else:
     coeffs[0] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "rb"))
-
 
 #Pm = 1E-4: Q = 0.747, Rm = 4.88
 Pm = 1.0E-4
 Q = 0.747
 Rm = 4.88
 
+
 if norm == False:
     coeffs[1] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+"_norm_"+str(norm)+".p", "rb"))
 else:
     coeffs[1] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "rb"))
+
 
 #Pm = 1E-3, Q = 0.75, Rm = 4.8775
 #gridnum = 256
@@ -71,7 +79,9 @@ gridnum = 64
 Pm = 1.0E-3
 Q = 0.75
 Rm = 4.8775
+
 #coeffs[2] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "rb"))
+
 #coeffs[0] = pickle.load(open("1E-3test/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "rb"))
 
 if norm == False:
@@ -90,7 +100,9 @@ Rm = 4.93
 coeffs[5] = pickle.load(open("pspace/coeffs_gridnum"+str(gridnum)+"_Pm_"+str(Pm)+"_Q_"+str(Q)+"_Rm_"+str(Rm)+"_q_"+str(q)+"_beta_"+str(beta)+".p", "rb"))
 """
 
-num = 3
+
+num = len(coeffs)
+
 a_arr = np.zeros(num, dtype=np.complex128)
 c_arr = np.zeros(num, dtype=np.complex128)
 b_arr = np.zeros(num, dtype=np.complex128)
@@ -190,7 +202,12 @@ def plot4():
         #if i == 1:
         #    print("i = 1")
         #else: 
-            ax1.plot(coeffs[i]["t_array"], coeffs[i]["alpha_array"], '.', label = coeffs[i]["Pm"])
+        
+        #if i == 3:
+        ax1.plot(coeffs[i]["t_array"], coeffs[i]["alpha_array"], '.', label = coeffs[i]["Pm"])
+        #else:
+        #    ax1.plot(coeffs[i]["t_array"], coeffs[i]["alpha_array"][:, 0], '.', label = coeffs[i]["Pm"])
+        
             #ax1.plot(coeffs[i]["t_array"], coeffs[i]["alpha_array"][:, 0], '.', label = coeffs[i]["Pm"])
             #ax1.loglog(coeffs[i]["Pm"], coeffs[i]["alpha_array"][-1][0], '.', label = coeffs[i]["Pm"])
             #ax1.semilogx(coeffs[i]["Pm"], coeffs[i]["c"]/coeffs[i]["a"], '.', label = coeffs[i]["Pm"])
@@ -236,6 +253,26 @@ def plot_widths_byhand():
     plt.loglog(Pms, widths, '.')
     plt.xlim(1E-8, 1E-2)
         
+
+def saturation_amp_from_coeffs(mag=False):
+    fig = plt.figure()
+    Pms = np.zeros(len(coeffs))
+    asat = np.zeros(len(coeffs), dtype = np.complex128)
+    for i in range(len(coeffs)):
+        Pms[i] = coeffs[i]["Pm"]
+        asat[i] = np.sqrt((coeffs[i]["b"]*1j*coeffs[i]["Q"] - coeffs[i]["g"]*1j*coeffs[i]["Q"]**3)/coeffs[i]["c"])
+        
+    print(Pms, asat)
+    
+    if mag == True:
+        plt.loglog(Pms, np.sqrt(asat.real**2 + asat.imag**2), "+", markersize = 10)
+    else:
+        plt.loglog(Pms, asat, "+", markersize = 10)
+    
+    plt.xlim(min(Pms) - min(Pms)/2, max(Pms) + max(Pms)/2)
+    plt.xlabel("Pm")
+    plt.ylabel(r"$\alpha_{saturation}$")
+
     
 def plot5():
     fig = plt.figure()

python/allorders.py
@@ -18,7 +18,7 @@ from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 
-gridnum = 512
+gridnum = 64
 x_basis = Chebyshev(gridnum)
 domain = Domain([x_basis], grid_dtype=np.complex128)
 
@@ -751,10 +751,10 @@ class OrderE2():
         self.term2_B = term2_B.evaluate()
         
         # righthand side for the 21 terms (e^iQz)
-        rhs21_psi = self.term2_psi['g']
-        rhs21_u = self.term2_u['g']
-        rhs21_A = self.term2_A['g']
-        rhs21_B = self.term2_B['g']
+        rhs21_psi = -self.term2_psi['g']   # changed to correct sign 1/23/15
+        rhs21_u = -self.term2_u['g']
+        rhs21_A = -self.term2_A['g']
+        rhs21_B = -self.term2_B['g']
                 
         # define problem using righthand side as nonconstant coefficients
         
@@ -2118,6 +2118,10 @@ class SolveAmplitudeAlpha():
         #problem.add_equation("-(a/c)*dt(alpha) - (b/c)*alphaZ + (h/c)*dZ(alphaZ) + (g/c)*1j*Q**3*alpha = alpha*Absolute(alpha**2)")
         
         problem.add_equation("ac*dt(alpha) - bc*1j*Q*alpha - hc*dZ(alphaZ) + gc*1j*Q**3*alpha = alpha*Absolute(alpha**2)") #fixed to be gle
+        
+        #problem.add_equation("ac*dt(alpha) - bc*1j*Q*alpha - hc*dZ(alphaZ) = alpha*Absolute(alpha**2)") #fixed to be gle
+        
+        
         #problem.add_equation("ac*dt(alpha) - bc*1j*Q*alpha - hc*dZ(alphaZ) = alpha*Absolute(alpha**2)") #fixed to be gle
         problem.add_equation("alphaZ - dZ(alpha) = 0")
         
@@ -2148,7 +2152,7 @@ class SolveAmplitudeAlpha():
         # stopping criteria
         solver.stop_sim_time = np.inf
         solver.stop_wall_time = np.inf
-        solver.stop_iteration = 500000
+        solver.stop_iteration = 50000#0
         
         # reference local grid and state fields
         Z = domain.grid(0)