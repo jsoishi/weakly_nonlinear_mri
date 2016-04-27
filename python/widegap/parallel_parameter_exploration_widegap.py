c0a7f6190303b5ed4f288654930614c396ce31e9
Susan Clark <sec2170@columbia.edu>
2016-04-27 14:11:58 -0400

    yet smaller Q

--- python/widegap/parallel_parameter_exploration_widegap.py
+++ python/widegap/parallel_parameter_exploration_widegap.py
@@ -282,9 +282,10 @@ if __name__ == '__main__':
     dRm = 0.05
     
     #Qsearch = np.arange(0.6, 0.9, dQ)
-    Qsearch = np.arange(0.2, 0.6, dQ)
-    #Rmsearch = np.arange(4.6, 5.1, dRm)
-    Rmsearch = np.arange(5.1, 6.0, dRm)
+    #Qsearch = np.arange(0.2, 0.6, dQ)
+    Qsearch = np.arange(-0.2, 0.2, dQ)
+    Rmsearch = np.arange(4.6, 5.1, dRm)
+    #Rmsearch = np.arange(5.1, 6.0, dRm)
     
     #Qsearch = np.arange(0.2, 0.4, dQ)
     #Qsearch = np.arange(0.74, 0.76, dQ)
