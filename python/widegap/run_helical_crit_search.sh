276f3ce2b9e013935ff6c5bbed549841b64fad16
Susan Clark <sec2170@columbia.edu>
2016-08-09 12:57:01 -0400

    different interval

--- python/widegap/run_helical_crit_search.sh
+++ python/widegap/run_helical_crit_search.sh
@@ -13,5 +13,5 @@ source /u/sclark9/dedalus/bin/activate
 export PYTHONPATH=$PYTHONPATH:$HOME/eigentools
 export MPLBACKEND='Agg'
 date
-mpiexec_mpt -np 400 python3 $dedalus_script.py --R1=1 --R2=2 --Omega1=313.55 --Omega2=87.794 --Pm=1.0E-6 --beta=0.0174 --xi=4.0 --Rm_min=1.0E-4 --Rm_max=1.0 --k_min=1.0E-1 --k_max=100.0 --insulate=1
+mpiexec_mpt -np 400 python3 $dedalus_script.py --R1=1 --R2=2 --Omega1=313.55 --Omega2=87.794 --Pm=1.0E-6 --beta=0.0174 --xi=4.0 --Rm_min=1.0E-4 --Rm_max=1.0E1 --k_min=1.0E-1 --k_max=50.0 --insulate=1
 date
