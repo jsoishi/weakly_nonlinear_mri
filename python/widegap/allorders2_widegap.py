running w/out normalization seems to fix spuriously large Re/Im part problems. trying w/ higher resolution, coefficients returned to thingap sign conventions

python/widegap/allorders2_widegap.py
@@ -836,7 +836,7 @@ class OrderE2(MRI):
         rrdu0field = self.domain.new_field()
         rrdu0field['g'] = rfield['g']**2*self.c1 - self.c2
         
-        print('N2 r', rfield['g'])
+        #print('N2 r', rfield['g'])
         
         # multiplied by r^4
         rhs_psi21 = (-self.iR*4*1j*self.Q**3*rfield**3*o1.psi - (2/self.beta)*3*rfield**3*self.Q**2*o1.A + self.iR*4*1j*self.Q*rfield**3*o1.psi_rr
@@ -1345,23 +1345,23 @@ class AmplitudeAlpha(MRI):
         a_psi_rhs = a_psi_rhs.evaluate()
         
         # b is the *opposite sign* as thin gap. .... but then they flip the sign. so same sign. 
-        # let's do the exact same thing... #actually let's not
+        # let's do the exact same thing... 
         # Gtwiddle . V11 - xi dz H . V11
         b_psi_rhs = -(2/self.beta)*invr*1j*self.Q**3*o1.A + (2/self.beta)*invr*1j*self.Q*o1.A_rr - (2/self.beta)*invr**2*1j*self.Q*o1.A_r - (2/self.beta)*invr**2*2*1j*self.Q*self.xi*o1.B
         #b_psi_rhs = b_psi_rhs.evaluate()
-        b_psi_rhs = b_psi_rhs.evaluate()
+        b_psi_rhs = (-b_psi_rhs).evaluate()
        
         b_u_rhs = (2/self.beta)*1j*self.Q*o1.B
         #b_u_rhs = b_u_rhs.evaluate()
-        b_u_rhs = b_u_rhs.evaluate()
+        b_u_rhs = (-b_u_rhs).evaluate()
         
         b_A_rhs = 1j*self.Q*o1.psi
         #b_A_rhs = b_A_rhs.evaluate()
-        b_A_rhs = b_A_rhs.evaluate()
+        b_A_rhs = (-b_A_rhs).evaluate()
         
         b_B_rhs = 1j*self.Q*o1.u + invr**3*2*1j*self.Q*self.xi*o1.psi
         #b_B_rhs = b_B_rhs.evaluate()
-        b_B_rhs = b_B_rhs.evaluate()
+        b_B_rhs = (-b_B_rhs).evaluate()
         
         # (L1twiddle V21 + L2twiddle V11 + xi H V21)
         h_psi_rhs = (self.iR*invr*4*1j*self.Q**3*o2.psi21 + (2/self.beta)*invr*3*self.Q**2*o2.A21 + self.iR*invr*6*self.Q**2*o1.psi - (2/self.beta)*invr*3*1j*self.Q*o1.A
@@ -1390,14 +1390,14 @@ class AmplitudeAlpha(MRI):
         #h_B_rhs = h_B_rhs.evaluate()
         
         #doing same thing as thin gap
-        #h_psi_rhs = (-h_psi_rhs).evaluate()
-        #h_u_rhs = (-h_u_rhs).evaluate()
-        #h_A_rhs = (-h_A_rhs).evaluate()
-        #h_B_rhs = (-h_B_rhs).evaluate()
-        h_psi_rhs = h_psi_rhs.evaluate()
-        h_u_rhs = h_u_rhs.evaluate()
-        h_A_rhs = h_A_rhs.evaluate()
-        h_B_rhs = h_B_rhs.evaluate()
+        h_psi_rhs = (-h_psi_rhs).evaluate()
+        h_u_rhs = (-h_u_rhs).evaluate()
+        h_A_rhs = (-h_A_rhs).evaluate()
+        h_B_rhs = (-h_B_rhs).evaluate()
+        #h_psi_rhs = h_psi_rhs.evaluate()
+        #h_u_rhs = h_u_rhs.evaluate()
+        #h_A_rhs = h_A_rhs.evaluate()
+        #h_B_rhs = h_B_rhs.evaluate()
         
         # Normalize s.t. a = 1
         #if norm is True:
@@ -1408,13 +1408,13 @@ class AmplitudeAlpha(MRI):
         self.a = self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [a_psi_rhs, o1.u, o1.A, o1.B])
         
         # c = <va . N31*>
-        #self.c = -self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [n3.N31_psi, n3.N31_u, n3.N31_A, n3.N31_B])
-        self.c = self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [n3.N31_psi, n3.N31_u, n3.N31_A, n3.N31_B])
+        self.c = -self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [n3.N31_psi, n3.N31_u, n3.N31_A, n3.N31_B])
+        #self.c = self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [n3.N31_psi, n3.N31_u, n3.N31_A, n3.N31_B])
         logger.info("self.c = {}".format(self.c))
         
         # b = < va . (Gtwiddle v11 - xi*dz*H v11)* > 
-        #self.b = -self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [b_psi_rhs, b_u_rhs, b_A_rhs, b_B_rhs])
-        self.b = self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [b_psi_rhs, b_u_rhs, b_A_rhs, b_B_rhs])
+        self.b = -self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [b_psi_rhs, b_u_rhs, b_A_rhs, b_B_rhs])
+        #self.b = self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [b_psi_rhs, b_u_rhs, b_A_rhs, b_B_rhs])
   
         # h = < va . ( L1twiddle V21 + L2twiddle V11 + xi H V21)* > 
         self.h = self.take_inner_product([ah.psi, ah.u, ah.A, ah.B], [h_psi_rhs, h_u_rhs, h_A_rhs, h_B_rhs])
@@ -1423,7 +1423,7 @@ class AmplitudeAlpha(MRI):
         self.linear_term = self.b
     
 
-        self.sat_amp_coeffs = np.sqrt(-self.b/self.c) #np.sqrt((-1j*self.Q*self.b + 1j*self.Q**3*self.g)/self.c)
+        self.sat_amp_coeffs = np.sqrt(self.b/self.c) #np.sqrt((-1j*self.Q*self.b + 1j*self.Q**3*self.g)/self.c)
         
         # For interactive diagnostic purposes only
         self.o1 = o1

python/widegap/run_widegap_amplitude.py
@@ -14,6 +14,7 @@ Omega2 = 0.12087
 beta = 41.2
 Pm = 1.6e-6
 xi=0
+norm = False
 
 # UMR07-like parameters
 #Q = 0.01269
@@ -60,12 +61,12 @@ r_basis = de.Chebyshev('r', gridnum, interval=(R1, R2))
 domain = de.Domain([r_basis], np.complex128, comm=MPI.COMM_SELF)
 print("running at gridnum", gridnum)
 
-aa = AmplitudeAlpha(domain, Q = Q, Rm = Rm, Pm = Pm, xi = xi, Omega1 = Omega1, Omega2 = Omega2, beta = beta, norm=False)
+aa = AmplitudeAlpha(domain, Q = Q, Rm = Rm, Pm = Pm, xi = xi, Omega1 = Omega1, Omega2 = Omega2, beta = beta, norm=norm)
 aa.print_coeffs()
 
 #fn_root = "/Users/susanclark/weakly_nonlinear_mri/data/"
 fn_root = "../../data/"
-fn = fn_root + "widegap_amplitude_parameters_Q_{:03.2f}_Rm_{:04.4f}_Pm_{:.2e}_Omega1_{:05.2f}_Omega2_{:05.2f}_beta_{:.2f}_xi_{:.2f}_gridnum_{}_normFalse.h5".format(Q, Rm, Pm, Omega1, Omega2, beta, xi, gridnum)
+fn = fn_root + "widegap_amplitude_parameters_Q_{:03.2f}_Rm_{:04.4f}_Pm_{:.2e}_Omega1_{:05.2f}_Omega2_{:05.2f}_beta_{:.2f}_xi_{:.2f}_gridnum_{}_norm_{}.h5".format(Q, Rm, Pm, Omega1, Omega2, beta, xi, gridnum, norm)
 with h5py.File(fn,'w') as f:
     r = f.create_dataset("r", data=aa.r)
     o1psi = f.create_dataset("psi11", data=aa.o1.psi['g'])