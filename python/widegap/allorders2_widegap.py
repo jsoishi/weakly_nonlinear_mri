8884e8f963e948ec993a16d8f15732ec80997b6d
Susan Clark <sec2170@columbia.edu>
2016-08-17 17:36:05 -0400

    N20 psi + c.c. and N20 B + c.c. need to be explicitly zero

--- python/widegap/allorders2_widegap.py
+++ python/widegap/allorders2_widegap.py
@@ -664,11 +664,16 @@ class OrderE2(MRI):
         N20_A_r_cc['g'] = n2.N20_A_r['g'].conj()
         N20_B_r2_cc = self.domain.new_field()
         N20_B_r2_cc['g'] = n2.N20_B_r2['g'].conj()
+        
+        allzeros = self.domain.new_field()
+        allzeros['g'] = np.zeros(len(rfield['g'], np.complex128)
     
-        self.rhs_psi20 = (-n2.N20_psi_r4 - N20_psi_r4_cc).evaluate()
+        #self.rhs_psi20 = (-n2.N20_psi_r4 - N20_psi_r4_cc).evaluate()
+        self.rhs_psi20 = allzeros
         self.rhs_u20 = (-n2.N20_u_r2 - N20_u_r2_cc).evaluate()
         self.rhs_A20 = (-n2.N20_A_r - N20_A_r_cc).evaluate()
-        self.rhs_B20 = (-n2.N20_B_r2 - N20_B_r2_cc).evaluate()
+        self.rhs_B20 = allzeros
+        #self.rhs_B20 = (-n2.N20_B_r2 - N20_B_r2_cc).evaluate()
     
     
         # V20 equations are separable because dz terms -> 0, but we'll solve them coupled anyway.
@@ -685,6 +690,7 @@ class OrderE2(MRI):
             bv20.parameters['bessel1'] = special.iv(0, self.Q*self.R1)/special.iv(1, self.Q*self.R1)
             bv20.parameters['bessel2'] = special.kn(0, self.Q*self.R2)/special.kn(1, self.Q*self.R2)
         
+        # these are multiplied by [r^4, r^2, r, r^2]
         bv20.add_equation("-iR*(r**3*dr(psirrr) - r**2*2*psirrr + r*3*psirr - 3*psir) = rhs_psi20")
         bv20.add_equation("-iR*(r**2*dr(ur) + r*ur - u) = rhs_u20")
         bv20.add_equation("-iRm*(r*dr(Ar) - Ar) = rhs_A20")
