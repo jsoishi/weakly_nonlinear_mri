04bcb416b636edeb316a4d73abb372c7868185a6
Susan Clark <sec2170@columbia.edu>
2016-08-18 08:53:38 -0400

    run critical eigenval search with Rm as eigenvalue on pleiades

---
+++ python/widegap/pleiades_run_critical_eigenvalue_search.sh
@@ -0,0 +1,17 @@
+#PBS -S /bin/bash
+#PBS -N CritEvalSearchbyRm
+#PBS -l select=2:ncpus=10:mpiprocs=10:model=has
+#PBS -l walltime=00:10:00
+#PBS -j oe
+#PBS -q devel
+
+dedalus_script=critical_eigenvalue_search_widegap
+
+cd $PBS_O_WORKDIR
+
+source /u/sclark9/dedalus/bin/activate
+export PYTHONPATH=$PYTHONPATH:$HOME/eigentools
+export MPLBACKEND='Agg'
+date
+mpiexec_mpt -np 20 python3 $dedalus_script.py
+date
