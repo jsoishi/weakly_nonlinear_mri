7e6125951e571c0d75b9e91dd0b358062c277e3c
Susan Clark <sec2170@columbia.edu>
2016-08-30 19:20:45 -0400

    run widegap fiducial at 512

--- python/widegap/pleiades_run_widegap_amplitude.sh
+++ python/widegap/pleiades_run_widegap_amplitude.sh
@@ -1,9 +1,9 @@
 #PBS -S /bin/bash
-#PBS -N WidegapFinal
+#PBS -N WidegapFinalLong
 #PBS -l select=2:ncpus=10:mpiprocs=10:model=has
-#PBS -l walltime=02:00:00
+#PBS -l walltime=20:00:00
 #PBS -j oe
-#PBS -q devel
+#PBS -q long
 
 dedalus_script=run_widegap_amplitude
 
