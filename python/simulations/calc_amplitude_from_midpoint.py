e8740a6a52ad02e1c4bd328de8038a28086b3dfb
J.S. Oishi <jsoishi@gmail.com>
2016-08-30 06:42:24 -0700

    use rms along z rather than center in both x,z to construct u amplitude

--- python/simulations/calc_amplitude_from_midpoint.py
+++ python/simulations/calc_amplitude_from_midpoint.py
@@ -9,7 +9,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-outfilename = "../../data/center_amplitude_vs_time.h5"
+outfilename = "../../data/rms_amplitude_vs_time.h5"
 basename = "scratch/MRI_run_Rm5.00e+00_eps0.00e+00_Pm{:5.2e}_beta2.50e+01_Q7.50e-01_qsh1.50e+00_Omega1.00e+00_nx128_nz128_Lz2_CFL"
 
 pms = np.array([3e-4,1e-3,3e-3])
@@ -22,7 +22,7 @@ for pm in pms:
 
     datafiles = glob.glob(str(f.joinpath("slices_s*.h5")))
     datafiles.sort(key=lambda m: int(re.match("[\D\d]*slices_s(\d+).h5",m).group(1)))
-    u_center = []
+    u_rms = []
     t = []
 
     for df in datafiles:
@@ -31,11 +31,12 @@ for pm in pms:
         with h5py.File(df,"r") as data:
             nt,nz,nx = data['/tasks/u'].shape
 
-            u_center.append(data['/tasks/u'][:,nz/2,nx/2])
+            urms_z = np.sqrt((data['/tasks/u'][:,:,nx/2]**2).sum(axis=1)/nz)
+            u_rms.append(urms_z)
             t.append(data['/scales/sim_time'][:])
-    datapath = str(pm) + "/u_center"
+    datapath = str(pm) + "/u_rms"
     scalepath = str(pm) + "/t"
-    outdata[datapath] = np.concatenate(u_center)
+    outdata[datapath] = np.concatenate(u_rms)
     outdata[scalepath] = np.concatenate(t)
 
 outdata.close()
