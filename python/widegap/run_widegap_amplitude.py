2b44088a3e30b345b4dd37a635f27030a8d4b9c6
Susan Clark <sec2170@columbia.edu>
2016-08-30 15:17:21 -0400

    512 version of Pm=1.6E-4

--- python/widegap/run_widegap_amplitude.py
+++ python/widegap/run_widegap_amplitude.py
@@ -102,7 +102,7 @@ Omega1 = 313.55
 Omega2 = Omega1*mu_omega
 """
 
-gridnum = 128
+gridnum = 512
 
 r_basis = de.Chebyshev('r', gridnum, interval=(R1, R2))
 domain = de.Domain([r_basis], np.complex128, comm=MPI.COMM_SELF)
