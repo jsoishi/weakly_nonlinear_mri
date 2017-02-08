56e461502d25303921331293c3d2289e3a371c4e
J.S. Oishi <jsoishi@gmail.com>
2017-02-08 09:01:20 -0500

    added smooth spectral filter.

--- python/simulations/filter_field.py
+++ python/simulations/filter_field.py
@@ -12,3 +12,17 @@ def filter_field(field,frac=0.5):
     for i in range(dom.dim):
         field_filter = field_filter | (cc[i][local_slice] > frac)
     field['c'][field_filter] = 0j
+
+def smooth_filter_field(field,frac=0.5,sharp=30.):
+    dom = field.domain
+    local_slice = dom.dist.coeff_layout.slices(scales=dom.dealias)
+    coeff = []
+    for i in range(dom.dim)[::-1]:
+        coeff.append(np.linspace(0,1,dom.global_coeff_shape[i],endpoint=False))
+    cc = np.meshgrid(*coeff)
+    
+    field_filter = np.ones(dom.local_coeff_shape,dtype='complex')
+    for i in range(dom.dim):
+        field_filter *= 0.5 - 0.5*np.tanh(sharp*(cc[i][local_slice]-0.5))
+    
+    field['c'] *= field_filter
