cc114cf62fae6f7d273ca9fe272eef584287ff9a
J.S. Oishi <jsoishi@gmail.com>
2017-03-09 13:58:02 -0800

    added one dimensional option for filter field.

--- python/simulations/filter_field.py
+++ python/simulations/filter_field.py
@@ -26,3 +26,20 @@ def smooth_filter_field(field,frac=0.5,sharp=30.):
         field_filter *= 0.5 - 0.5*np.tanh(sharp*(cc[i][local_slice]-0.5))
     
     field['c'] *= field_filter
+
+def filter_field_onedim(field,dim, frac=0.5):
+    """
+    filter field only in the dim direction
+    """
+
+    dom = field.domain
+    local_slice = dom.dist.coeff_layout.slices(scales=dom.dealias)
+    coeff = []
+    for i in range(dom.dim)[::-1]:
+        coeff.append(np.linspace(0,1,dom.global_coeff_shape[i],endpoint=False))
+    cc = np.meshgrid(*coeff)
+    
+    field_filter = np.zeros(dom.local_coeff_shape,dtype='bool')
+    field_filter = field_filter | (cc[dim][local_slice] > frac)
+    field['c'][field_filter] = 0j
+
