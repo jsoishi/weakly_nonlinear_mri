ff2a1e62a5b2c1b9d9653ccb75df5446d0c00594
Susan Clark <sec2170@columbia.edu>
2017-03-13 20:56:35 -0400

    run in widegap folder

---
+++ python/widegap/pleiades_run_widegap_amplitude_bro_ele.sh
@@ -0,0 +1,17 @@
+#PBS -S /bin/bash
+#PBS -N WidegapFinalLongBroEle
+#PBS -l select=2:ncpus=28:mpiprocs=28:model=bro_ele
+#PBS -l walltime=20:00:00
+#PBS -j oe
+#PBS -q long
+
+dedalus_script=run_widegap_amplitude
+
+cd $PBS_O_WORKDIR
+
+source /u/sclark9/dedalus/bin/activate
+export PYTHONPATH=$PYTHONPATH:$HOME/eigentools
+export MPLBACKEND='Agg'
+date
+mpiexec_mpt -np 20 python3 $dedalus_script.py
+date
