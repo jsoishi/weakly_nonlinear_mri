f7b7a43c29d5438fda81fa861526e5681a8e66ce
susanclark <susanclark@localhost>
2017-09-22 21:21:33 -0400

    critical finder showing weird behavior

--- python/hmrisims/run_find_hmri_crit_sparse.py
+++ python/hmrisims/run_find_hmri_crit_sparse.py
@@ -19,11 +19,11 @@ from find_hmri_crit_sparse import find_crit
 
 # First we need critical values as computed by Rainer
 #Pms = [1, 0.1, 0.01, 0.001, 0.0001]
-Pms = [1, 0.1, 0.01, 0.001, 0.0001]
+Pms = [0.0001, 0.001, 0.01, 0.1, 1]
 xis = [0, 1, 2, 3, 4, 5]
 
 # Set up file names
-root = "/Users/susanclark/Projects/HMRISims/data/Results/"
+root = "/Users/susanclark/Projects/HMRISims/data/New1D/"
 
 # Fixed parameters
 nr = 64
@@ -32,8 +32,8 @@ R2 = 2
 Omega1 = 1
 Omega2 = 0.27
 
-n_Rm = 10
-n_k = 10
+n_Rm = 11
+n_k = 11
 
 insulate = True
     
@@ -42,17 +42,18 @@ for Pm in Pms:
     all_params_dict = {}
     
     if Pm > 0.001:
-        #xis = [0, 1, 2, 3, 4, 5]
-        xis=[1]
+        xis = [0, 1, 2, 3, 4, 5]
     
     # Smallest Pms only have xi >= 3
     elif Pm <= 0.001: 
-        xis = [3, 4, 5]
+        #xis = [3, 4, 5]
+        xis = [4]
     
     for xi in xis:
         print("Finding critical parameters for Pm={}, xi={}".format(Pm, xi))
         
-        fortfn = root+"Pm"+str(Pm)+"/beta"+str(xi)+"/fort.99"
+        #fortfn = root+"Pm"+str(Pm)+"/beta"+str(xi)+"/fort.99"
+        fortfn = root+"Pm"+str(Pm)+"/beta"+str(xi)+"/ConstantLambda1Dcut"
         fortdata = np.loadtxt(fortfn)
         (Ha, Re, k_optimal, sigma_Re, sigma_Im) = fortdata[0]
         
@@ -62,16 +63,26 @@ for Pm in Pms:
         beta = (2*Re*Rm)/Ha**2
         
         print("Rainer's Rm = {}, beta = {}, Co = {}".format(Rm, beta, 2.0/beta))
+        
+        # make bounds based on Ha instead, since Rm is a nonlinear func of Ha.
+        Ha_min = Ha - 0.007*Ha
+        Ha_max = Ha + 0.001*Ha
+        
+        Rm_1 = (beta*Ha_min**2)/(2*Re)
+        Rm_2 = (beta*Ha_max**2)/(2*Re)
+        
+        Rm_min = min(Rm_1, Rm_2)
+        Rm_max = max(Rm_1, Rm_2)
 
         #Rm_min = max(Rm - 10*Rm, 0.01)
-        Rm_min = Rm - 0.9*Rm
-        Rm_max = Rm + 0.9*Rm
+        #Rm_min = Rm - 0.005*Rm
+        #Rm_max = Rm + 0.01*Rm
         print("Rm min is {}, Rm max is {}".format(Rm_min, Rm_max))
         print("that is, Ha min = {}, Ha max = {}".format(np.sqrt((2*Re*Rm_min)/beta), np.sqrt((2*Re*Rm_max)/beta)))
         
         #k_min = max(k_optimal - 10*k_optimal, 0.01)
-        k_min = k_optimal - 0.9*k_optimal
-        k_max = k_optimal + 0.9*k_optimal
+        k_min = k_optimal - 0.015*k_optimal
+        k_max = k_optimal + 0.005*k_optimal
         print("k min is {}, k max is {}".format(k_min, k_max))
     
         Q, Rmc, omega = find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min, k_max, n_Rm, n_k, nr, insulate)
@@ -81,7 +92,7 @@ for Pm in Pms:
             
         all_params_dict[(Pm, xi)] = (Q, Rmc, omega, Ha_crit, Re, beta, insulate)
         
-    #np.save(root+"Pm"+str(Pm)+"/Pm_{}_all_xi_Omega2_{}_critical_parameters.npy".format(str(Pm)), all_params_dict, Omega2) 
+    np.save(root+"Pm"+str(Pm)+"/Pm_{}_all_xi_Omega2_{}_critical_parameters.npy".format(str(Pm)), all_params_dict, Omega2) 
 
 
 
