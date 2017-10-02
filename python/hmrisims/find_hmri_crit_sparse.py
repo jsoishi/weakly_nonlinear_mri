d35a2f5dc3269b3995e20b61029e26ca1cca2bda
susanclark <susanclark@localhost>
2017-10-02 22:16:34 +0200

    hack to make bessel funcs update with each k in grid by including them in shim and Re updates because now it is a substitution rather than parameter

--- python/hmrisims/find_hmri_crit_sparse.py
+++ python/hmrisims/find_hmri_crit_sparse.py
@@ -22,6 +22,8 @@ def find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min,
 
     c1 = (Omega2*R2**2 - Omega1*R1**2)/(R2**2 - R1**2)
     c2 = (R1**2*R2**2*(Omega1 - Omega2))/(R2**2 - R1**2)
+    
+    print("c1 = {}, c2 = {}".format(c1, c2))
 
     zeta_mean = 2*(R2**2*Omega2 - R1**2*Omega1)/((R2**2 - R1**2)*np.sqrt(Omega1*Omega2))
     if comm.rank == 0:
@@ -34,12 +36,16 @@ def find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min,
 
     r = de.Chebyshev('r', nr, interval = (R1, R2))
     d = de.Domain([r],comm=MPI.COMM_SELF)
+    
+    Co = 2.0/beta
+    Re = Rm/Pm
 
     widegap = de.EVP(d,['psi','u', 'A', 'B', 'psir', 'psirr', 'psirrr', 'ur', 'Ar', 'Br'],'sigma')
 
     widegap.parameters['k'] = k
-    widegap.parameters['iR'] = iR
     widegap.parameters['Rm'] = Rm # Rm rather than iRm so search options are more intuitive
+    #widegap.parameters['iR'] = Pm/widegap.parameters['Rm']#1/Re#iR
+    widegap.parameters['Pm'] = Pm
     widegap.parameters['c1'] = c1
     widegap.parameters['c2'] = c2
     widegap.parameters['beta'] = beta
@@ -52,6 +58,7 @@ def find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min,
     widegap.substitutions['ru0'] = '(r*r*c1 + c2)' # u0 = r Omega(r) = Ar + B/r
     widegap.substitutions['rrdu0'] = '(c1*r*r-c2)' # du0/dr = A - B/r^2
     widegap.substitutions['twooverbeta'] = '(2.0/beta)'
+    widegap.substitutions['iR'] = '(Pm/Rm)'
     widegap.substitutions['psivisc'] = '(2*r**2*k**2*psir - 2*r**3*k**2*psirr + r**3*k**4*psi + r**3*dr(psirrr) - 3*psir + 3*r*psirr - 2*r**2*psirrr)'
     widegap.substitutions['uvisc'] = '(-r**3*k**2*u + r**3*dr(ur) + r**2*ur - r*u)'
     widegap.substitutions['Avisc'] = '(r*dr(Ar) - r*k**2*A - Ar)' 
@@ -96,7 +103,10 @@ def find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min,
 
     # create a shim function to translate (x, y) to the parameters for the eigenvalue problem:
     def shim(x,y):
-        gr, indx,freq = EP.growth_rate({"k":x,"Rm":y})
+        gr, indx,freq = EP.growth_rate({"k":x,"Rm":y, "bessel1":special.iv(0, x*R1)/special.iv(1, x*R1), "bessel2":special.kn(0, x*R2)/special.kn(1, x*R2)})
+        #EP.spectrum(title='../figs/good_eigenvalue_spectrum_k_{}_Rm_{}'.format(k, Rm),spectype='good')
+        print("Co = {}, Rm = {}, k = {}, Re = {}, Pm = {}, xi = {}".format(2/EP.EVP.parameters['beta'], EP.EVP.parameters['Rm'], EP.EVP.parameters['k'], EP.EVP.parameters['Pm']/EP.EVP.parameters['Rm'], Pm, EP.EVP.parameters['xi']))
+        print("Growth rate: {} freq: {}".format(gr, freq))
         return gr+1j*freq
 
     cf = CriticalFinder(shim, comm)
@@ -116,6 +126,17 @@ def find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min,
                 gridname = '../data/hmri_growth_rates_res{0:d}_Pm{1:5.02e}_Rmmin{2:5.02e}_Rmmax{3:5.02e}_kmin{4:5.02e}_kmax{5:5.02e}_nRm{6:5.02e}_nk{7:5.02e}_Omega1_{8:5.02e}_Omega2_{9:5.02e}_R1_{10:5.02e}_R2_{11:5.02e}_insulating'.format(nr,Pm, Rm_min,Rm_max, k_min, k_max, n_Rm, n_k, Omega1, Omega2, R1, R2)
         
         cf.save_grid(gridname)
+        
+        # plot grid regardless of whether critical value is found
+        if xi == 0:
+            title_str = '../figs/widegap_growth_rates_Pm{0:.4f}_xi0_res{0:d}_Rmmin{1:5.02e}_Rmmax{2:5.02e}_kmin{3:5.02e}_kmax{4:5.02e}_nRm{5:5.02e}_nk{6:5.02e}'.format(Pm, nr,Rm_min,Rm_max, k_min, k_max, n_Rm, n_k)
+            title_str_freq = '../figs/widegap_Im_sigma_Pm{0:.4f}_xi0_res{1:f}_Rmmin{2:5.02e}_Rmmax{3:5.02e}_kmin{4:5.02e}_kmax{5:5.02e}_nRm{6:5.02e}_nk{7:5.02e}_Co{8}'.format(Pm, nr,Rm_min,Rm_max, k_min, k_max, n_Rm, n_k, Co)
+        
+        else:
+            title_str = '../figs/helical_growth_rates_Pm{0:.4f}_xi{1:d}_res{2:f}_Rmmin{3:5.02e}_Rmmax{4:5.02e}_kmin{5:5.02e}_kmax{6:5.02e}_nRm{7:5.02e}_nk{8:5.02e}_Co{9}'.format(Pm, xi, nr,Rm_min,Rm_max, k_min, k_max, n_Rm, n_k, Co)
+            title_str_freq = '../figs/helical_Im_sigma_Pm{0:.4f}_xi{1:d}_res{2:f}_Rmmin{3:5.02e}_Rmmax{4:5.02e}_kmin{5:5.02e}_kmax{6:5.02e}_nRm{7:5.02e}_nk{8:5.02e}_Co{9}'.format(Pm, xi, nr,Rm_min,Rm_max, k_min, k_max, n_Rm, n_k, Co)
+        cf.plot_crit(title = title_str, xlabel = r"$k_z$", ylabel = r"$\mathrm{Rm}$", plotroots=False)
+        cf.plot_crit(title = title_str_freq, xlabel = r"$k_z$", ylabel = r"$\mathrm{Rm}$", plotroots=False, plotfreq=True)
 
     cf.root_finder()
     crit = cf.crit_finder(find_freq = True)
@@ -125,11 +146,7 @@ def find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min,
         print("critical omega = {:10.5f}".format(crit[2]))
         print("critical wavenumber k = {:10.5f}".format(crit[0]))
         print("critical Rm = {:10.5f}".format(crit[1]))
-        if xi == 0:
-            title_str = '../figs/widegap_growth_rates_Pm{}_xi{}_res{0:d}_Rmmin{1:5.02e}_Rmmax{2:5.02e}_kmin{3:5.02e}_kmax{4:5.02e}_nRm{5:5.02e}_nk{6:5.02e}'.format(Pm, xi, nr,Rm_min,Rm_max, k_min, k_max, n_Rm, n_k)
-        else:
-            title_str = '../figs/helical_growth_rates_Pm{}_xi{}_res{0:d}_Rmmin{1:5.02e}_Rmmax{2:5.02e}_kmin{3:5.02e}_kmax{4:5.02e}_nRm{5:5.02e}_nk{6:5.02e}'.format(Pm, xi, nr,Rm_min,Rm_max, k_min, k_max, n_Rm, n_k)
-        cf.plot_crit(title = title_str, xlabel = r"$k_z$", ylabel = r"$\mathrm{Rm}$")
+
     Q  = crit[0]
     Rmc = crit[1]
     omega = crit[2]
--- python/hmrisims/run_find_hmri_crit_sparse.py
+++ python/hmrisims/run_find_hmri_crit_sparse.py
@@ -20,20 +20,22 @@ from find_hmri_crit_sparse import find_crit
 # First we need critical values as computed by Rainer
 #Pms = [1, 0.1, 0.01, 0.001, 0.0001]
 Pms = [0.0001, 0.001, 0.01, 0.1, 1]
+Pms = [0.0001]
 xis = [0, 1, 2, 3, 4, 5]
 
 # Set up file names
 root = "/Users/susanclark/Projects/HMRISims/data/New1D/"
+root2 = "/Users/susanclark/Projects/HMRISims/data/GRIDS/"
 
 # Fixed parameters
-nr = 64
+nr = 32
 R1 = 1
 R2 = 2
 Omega1 = 1
 Omega2 = 0.27
 
-n_Rm = 11
-n_k = 11
+#n_Rm = 15
+#n_k = 11
 
 insulate = True
     
@@ -42,7 +44,8 @@ for Pm in Pms:
     all_params_dict = {}
     
     if Pm > 0.001:
-        xis = [0, 1, 2, 3, 4, 5]
+        #xis = [0, 1, 2, 3, 4, 5]
+        xis = [0]
     
     # Smallest Pms only have xi >= 3
     elif Pm <= 0.001: 
@@ -53,36 +56,96 @@ for Pm in Pms:
         print("Finding critical parameters for Pm={}, xi={}".format(Pm, xi))
         
         #fortfn = root+"Pm"+str(Pm)+"/beta"+str(xi)+"/fort.99"
-        fortfn = root+"Pm"+str(Pm)+"/beta"+str(xi)+"/ConstantLambda1Dcut"
-        fortdata = np.loadtxt(fortfn)
-        (Ha, Re, k_optimal, sigma_Re, sigma_Im) = fortdata[0]
+        #fortfn = root+"Pm"+str(Pm)+"/beta"+str(xi)+"/ConstantLambda1Dcut"
+        #fortdata = np.loadtxt(fortfn)
+        #(Ha, Re, k_optimal, sigma_Re, sigma_Im) = fortdata[0]
         
-        print("Data from {}: Ha={}, Re={}, k_opt={}, sig_Re={}, sig_Im={}".format(fortfn, Ha, Re, k_optimal, sigma_Re, sigma_Im))
+        # rainer provided new data 9/26
+        fortfn2 = root2+"Pm"+str(Pm)+"/beta"+str(xi)+"/fort.4"
+        fortdata2 = np.loadtxt(fortfn2)
+        
+        Rmkgrid = np.loadtxt(root2+"Pm"+str(Pm)+"/beta"+str(xi)+"/RmkGrid")
+        print("min k: {} max k: {}".format(np.nanmin(Rmkgrid[:, 2]), np.nanmax(Rmkgrid[:, 2])))
+        print("min Rm: {} max Rm: {}".format(np.nanmin(Rmkgrid[:, 1]), np.nanmax(Rmkgrid[:, 1])))
+        print("Rmk grid Co = {}".format(Rmkgrid[0, 0]))
+        
+        #print("Data from {}: Ha={}, Re={}, k_opt={}, sig_Re={}, sig_Im={}".format(fortfn, Ha, Re, k_optimal, sigma_Re, sigma_Im))
+        
+        (Ha, Re, k_optimal) = fortdata2     
+        print("New data: Ha={}, Re={}, k_opt={}".format(Ha, Re, k_optimal))
+        
+        
+        # define grid based on Rainer's
+        n_Rm = len(np.unique(Rmkgrid[:, 1]))
+        n_k = len(np.unique(Rmkgrid[:, 2]))
+        print("n_Rm = {}, n_k = {}".format(n_Rm, n_k))
+        
+        # plot Rainer's data on grid
+        gridRm = Rmkgrid[:, 1]
+        gridk = Rmkgrid[:, 2]
+        growthratevals = Rmkgrid[:, 3]
+        Imsigmas = Rmkgrid[:, 4]
+        dRm = sorted(gridRm)[-1] - sorted(gridRm)[-n_k-1]
+        dk = sorted(gridk)[-1] - sorted(gridk)[-n_Rm-1]
+        iRm = ((gridRm - np.nanmin(gridRm)) / dRm).astype(int)
+        jk = ((gridk - np.nanmin(gridk)) / dk).astype(int)
+        gridvals = np.zeros((n_Rm, n_k), np.float_)
+        gridvals[iRm, jk] = growthratevals
+        gridImsigmas = np.zeros((n_Rm, n_k), np.float_)
+        gridImsigmas[iRm, jk] = Imsigmas
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.pcolormesh(np.unique(gridk), np.unique(gridRm), gridvals, cmap="autumn")
+        fig.colorbar(cax)
+        plt.xlim(np.nanmin(gridk), np.nanmax(gridk))
+        plt.ylim(np.nanmin(gridRm), np.nanmax(gridRm))
+        plt.xlabel("k")
+        plt.ylabel("Rm")
+        plt.title("Rainer's growth rates for Pm {}, xi {}".format(Pm, xi))
+        plt.savefig("../figs/Rainer_growth_rates_Pm_{}_xi_{}.png".format(Pm, xi))
+        
+        # plot Im(sigma)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.pcolormesh(np.unique(gridk), np.unique(gridRm), gridImsigmas, cmap="autumn")
+        fig.colorbar(cax)
+        plt.xlim(np.nanmin(gridk), np.nanmax(gridk))
+        plt.ylim(np.nanmin(gridRm), np.nanmax(gridRm))
+        plt.xlabel("k")
+        plt.ylabel("Rm")
+        plt.title("Rainer's Im(sigma) for Pm {}, xi {}".format(Pm, xi))
+        plt.savefig("../figs/Rainer_Im_sigma_Pm_{}_xi_{}.png".format(Pm, xi))
+        
+        
+        #n_Rm = 3
+        #n_k = 3
         
         Rm = Pm*Re
         beta = (2*Re*Rm)/Ha**2
+        #beta = (Re*Rm)/(Omega2*Ha**2)
         
         print("Rainer's Rm = {}, beta = {}, Co = {}".format(Rm, beta, 2.0/beta))
         
         # make bounds based on Ha instead, since Rm is a nonlinear func of Ha.
-        Ha_min = Ha - 0.007*Ha
-        Ha_max = Ha + 0.001*Ha
+        #Ha_min = Ha - 0.05*Ha
+        #Ha_max = Ha + 0.05*Ha
         
-        Rm_1 = (beta*Ha_min**2)/(2*Re)
-        Rm_2 = (beta*Ha_max**2)/(2*Re)
+        #Rm_1 = (beta*Ha_min**2)/(2*Re)
+        #Rm_2 = (beta*Ha_max**2)/(2*Re)
         
-        Rm_min = min(Rm_1, Rm_2)
-        Rm_max = max(Rm_1, Rm_2)
+        #Rm_min = min(Rm_1, Rm_2)
+        #Rm_max = max(Rm_1, Rm_2)
 
         #Rm_min = max(Rm - 10*Rm, 0.01)
-        #Rm_min = Rm - 0.005*Rm
-        #Rm_max = Rm + 0.01*Rm
+        Rm_min = Rm - 0.01*Rm
+        Rm_max = Rm + 0.1*Rm
         print("Rm min is {}, Rm max is {}".format(Rm_min, Rm_max))
         print("that is, Ha min = {}, Ha max = {}".format(np.sqrt((2*Re*Rm_min)/beta), np.sqrt((2*Re*Rm_max)/beta)))
         
         #k_min = max(k_optimal - 10*k_optimal, 0.01)
-        k_min = k_optimal - 0.015*k_optimal
-        k_max = k_optimal + 0.005*k_optimal
+        k_min = k_optimal - 0.2*k_optimal
+        k_max = k_optimal + 0.2*k_optimal
         print("k min is {}, k max is {}".format(k_min, k_max))
     
         Q, Rmc, omega = find_crit(comm, R1, R2, Omega1, Omega2, beta, xi, Pm, Rm_min, Rm_max, k_min, k_max, n_Rm, n_k, nr, insulate)
@@ -92,7 +155,7 @@ for Pm in Pms:
             
         all_params_dict[(Pm, xi)] = (Q, Rmc, omega, Ha_crit, Re, beta, insulate)
         
-    np.save(root+"Pm"+str(Pm)+"/Pm_{}_all_xi_Omega2_{}_critical_parameters.npy".format(str(Pm)), all_params_dict, Omega2) 
+        np.save(root+"Pm"+str(Pm)+"/Pm_{0}_xi_{1}_Omega2_{2}_critical_parameters.npy".format(Pm, xi, Omega2), all_params_dict, Omega2) 
 
 
 
